82f79f5a39747e4a22929f737539d43492aaaea5
Wahed Mangal <mangal.wahed@gmail.com>
2021-03-23 17:25:06 +0100

    add rescript/react

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 import 'virtual:windi.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import './src/index.bs'
